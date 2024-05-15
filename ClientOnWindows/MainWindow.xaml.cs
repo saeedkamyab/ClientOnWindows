628ce4b5683732388f183ad3d1060b8892cea83b
saeedkamyab <saeed.kamyab@yahoo.com>
2024-05-15 18:15:37 +0330

    Add project files.

---
+++ ClientOnWindows/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using ClientOnWindows.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Newtonsoft.Json;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Windows;
+
+
+namespace ClientOnWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            Login loginModel = new Login();
+            loginModel.UserName = "admin";
+            loginModel.Password = "1234";
+            var client = new HttpClient { BaseAddress = new Uri("https://localhost:7102/api/") };
+            string myToken = "";
+
+
+
+            var jsonBody = JsonConvert.SerializeObject(loginModel);
+
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+            var response = client.PostAsync("Auth", content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                myToken = response.Content.ReadAsStringAsync().Result;
+                var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier,loginModel.UserName),
+                    new Claim(ClaimTypes.Name,loginModel.UserName),
+                    new Claim("AccessToken",myToken),
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+            }
+
+
+
+
+            // Create HttpClient
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", myToken);
+          
+            //client.DefaultRequestHeaders.Add("Authorization", "Bearer " + myToken);
+            
+            
+            // Assign default header (Json Serialization)
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ApiConstant.JsonHeader));
+
+            // Make an API call and receive HttpResponseMessage
+            var responseMessage = client.GetAsync("https://localhost:7102/api/Users").Result;
+
+            // Convert the HttpResponseMessage to string
+            var resultArray = responseMessage.Content.ReadAsStringAsync().Result;
+
+            // Deserialize the Json string into type using JsonConvert
+            var personList = JsonConvert.DeserializeObject<List<User>>(resultArray);
+
+            if (personList != null)
+                foreach (var item in personList)
+                {
+                    ListData.Items.Add("User Name: " + item.UserName + "  Full Name: " + item.FullName + "  F Name: " + item.FName);
+                }
+
+        }
+
+        //private void BtnLoadData_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //    // Create HttpClient
+        //    var client = new HttpClient { BaseAddress = new Uri("https://localhost:7102/api/") };
+
+        //    // Assign default header (Json Serialization)
+        //    //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ApiConstant.JsonHeader));
+
+        //    // Make an API call and receive HttpResponseMessage
+        //    var responseMessage =  client.GetAsync("Users", HttpCompletionOption.ResponseContentRead).Result;
+
+        //    // Convert the HttpResponseMessage to string
+        //    var resultArray = responseMessage.Content.ReadAsStringAsync().Result;
+
+        //    // Deserialize the Json string into type using JsonConvert
+        //    var personList = JsonConvert.DeserializeObject<List<User>>(resultArray);
+
+        //    foreach(var item in personList)
+        //    {
+        //        ListData.Items.Add("User Name: "+item.UserName+ "  Full Name: " + item.FullName+ "  F Name: "+item.FName);
+        //    }
+
+        //}
+    }
+}
---
+++ ClientOnWindows/Models/User.cs
@@ -0,0 +1,59 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientOnWindows.Models
+{
+    public class User
+    {
+
+        public int Id { get;  set; }
+        public string UserName { get;  set; }
+        public string Password { get;  set; }
+        [NotMapped]
+        public string RePassword { get;  set; }
+        public string FullName { get;  set; }
+        public string FName { get;  set; }
+
+        public string ProfilePhoto { get;  set; }
+        [NotMapped]
+        //public IFormFile? ProfilePhotoFile { get; set; }
+
+        public int RoleId { get; set; }
+        //public Role Role { get; set; }
+
+        public User()
+        {
+            
+        }
+
+        public User(string userName, string password, string fullName, string fName, string profilePhoto
+           , int roleId)
+        {
+            UserName = userName;
+            Password = password;
+            FullName = fullName;
+            FName = fName;
+            ProfilePhoto = profilePhoto;
+            RoleId = roleId;
+        }
+
+
+
+
+
+        public void Edit(string userName, string password, string fullName, string fName, string profilePhoto, int roleId)
+        {
+            UserName = userName;
+            FullName = fullName;
+            FName = fName;
+
+            //RoleId = roleId;
+
+            if (!string.IsNullOrWhiteSpace(profilePhoto))
+                ProfilePhoto = profilePhoto;
+            if (!string.IsNullOrWhiteSpace(password))
+                Password = password;
+        }
+
+    }
+}
