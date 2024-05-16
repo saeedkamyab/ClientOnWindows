9af57ac93e62f6d3964aff0d7ab6dc4e3b73dc28
saeedkamyab <saeed.kamyab@yahoo.com>
2024-05-16 09:09:02 +0330

    Client Side Finished

--- ClientOnWindows/MainWindow.xaml.cs
+++ ClientOnWindows/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text;
 using System.Windows;
+using System.Windows.Documents;
 
 
 namespace ClientOnWindows
@@ -16,93 +17,54 @@ namespace ClientOnWindows
     /// </summary>
     public partial class MainWindow : Window
     {
+        static HttpClient client = new HttpClient();
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void BtnLoadData_Click(object sender, RoutedEventArgs e)
+        static async Task<string> GetJwtTokenAsync(string username, string password, string authUrl)
         {
             Login loginModel = new Login();
-            loginModel.UserName = "admin";
-            loginModel.Password = "1234";
-            var client = new HttpClient { BaseAddress = new Uri("https://localhost:7102/api/") };
-            string myToken = "";
+            loginModel.UserName = username;
+            loginModel.Password = password;
 
 
+            var content = new StringContent(JsonConvert.SerializeObject(loginModel), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(authUrl, content);
+            response.EnsureSuccessStatusCode();
 
-            var jsonBody = JsonConvert.SerializeObject(loginModel);
+            // دریافت و پردازش توکن JWT
+            var responseString = await response.Content.ReadAsStringAsync();
+            var jwtToken = responseString;
 
-            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
-            var response = client.PostAsync("Auth", content).Result;
-            if (response.IsSuccessStatusCode)
+            return jwtToken;
+        }
+        static async Task<string> SendRequestWithJwtToken(string apiUrl, string token)
+        {
+            // افزودن توکن به Header درخواست
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            // ارسال درخواست به API و دریافت پاسخ
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+
+            // پردازش و چاپ پاسخ
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+        private async void BtnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            string apiUrl = "https://localhost:7102/api/Users";
+            string myToken = await GetJwtTokenAsync("admin", "1234", "https://localhost:7102/api/Auth");
+
+            var personList = JsonConvert.DeserializeObject<List<User>>(await SendRequestWithJwtToken(apiUrl, myToken));
+
+            foreach (var item in personList)
             {
-                myToken = response.Content.ReadAsStringAsync().Result;
-                var claims = new List<Claim>()
-                {
-                    new Claim(ClaimTypes.NameIdentifier,loginModel.UserName),
-                    new Claim(ClaimTypes.Name,loginModel.UserName),
-                    new Claim("AccessToken",myToken),
-                };
-
-                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                var principal = new ClaimsPrincipal(identity);
-
+                ListData.Items.Add("User Name: " + item.UserName + "  Full Name: " + item.FullName + "  F Name: " + item.FName);
             }
 
-
-
-
-            // Create HttpClient
-
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", myToken);
-          
-            //client.DefaultRequestHeaders.Add("Authorization", "Bearer " + myToken);
-            
-            
-            // Assign default header (Json Serialization)
-            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ApiConstant.JsonHeader));
-
-            // Make an API call and receive HttpResponseMessage
-            var responseMessage = client.GetAsync("https://localhost:7102/api/Users").Result;
-
-            // Convert the HttpResponseMessage to string
-            var resultArray = responseMessage.Content.ReadAsStringAsync().Result;
-
-            // Deserialize the Json string into type using JsonConvert
-            var personList = JsonConvert.DeserializeObject<List<User>>(resultArray);
-
-            if (personList != null)
-                foreach (var item in personList)
-                {
-                    ListData.Items.Add("User Name: " + item.UserName + "  Full Name: " + item.FullName + "  F Name: " + item.FName);
-                }
-
         }
-
-        //private void BtnLoadData_Click(object sender, RoutedEventArgs e)
-        //{
-
-        //    // Create HttpClient
-        //    var client = new HttpClient { BaseAddress = new Uri("https://localhost:7102/api/") };
-
-        //    // Assign default header (Json Serialization)
-        //    //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ApiConstant.JsonHeader));
-
-        //    // Make an API call and receive HttpResponseMessage
-        //    var responseMessage =  client.GetAsync("Users", HttpCompletionOption.ResponseContentRead).Result;
-
-        //    // Convert the HttpResponseMessage to string
-        //    var resultArray = responseMessage.Content.ReadAsStringAsync().Result;
-
-        //    // Deserialize the Json string into type using JsonConvert
-        //    var personList = JsonConvert.DeserializeObject<List<User>>(resultArray);
-
-        //    foreach(var item in personList)
-        //    {
-        //        ListData.Items.Add("User Name: "+item.UserName+ "  Full Name: " + item.FullName+ "  F Name: "+item.FName);
-        //    }
-
-        //}
     }
 }
